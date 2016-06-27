92fd366c8a27cde8265362bc1991551cb8aee53b
Amine <amineghozlene@gmail.com>
2016-06-27 09:55:16 +0100

    ajout du login

---
+++ mysqlAppWeb/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using mysqlAppWeb.Models;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mysqlAppWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(String id)
+        {
+            try
+            {
+                using (ISession session = NhibernateSession.OpenSession())
+                {
+                    var personne = session.Get<person>(id);
+                    return RedirectToAction("Personne/Index");
+                }
+            }catch(Exception e)
+            {
+                return View();
+            }
+        }
+    }
+}
--- mysqlAppWeb/Controllers/PersonneController.cs
+++ mysqlAppWeb/Controllers/PersonneController.cs
@@ -11,6 +11,11 @@ namespace mysqlAppWeb.Controllers
 {
     public class PersonneController : Controller
     {
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
         public ActionResult Create()
         {
             return View();
